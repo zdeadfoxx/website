23f9cdc1d31c2e57549dac3a5ab3be31452c26af
zdeadfoxx <54523548+zdeadfoxx@users.noreply.github.com>
2022-04-14 19:03:02 +0700

    there

--- index.php
+++ index.php
@@ -13,29 +13,9 @@
     <div class="wrapper">
         <header class="header">
             <div class="container">
-                <div class="header__body">
-                    <div class="header__logo">
-                        <img src="img/svg/logo.svg" alt="logo">
-                    </div>
-                    <nav class="header__nav">
-                        <ul class="menu">
-                            <li><a href="#" class="menu__item">Новости</a></li>
-                            <li><a href="#" class="menu__item">Игры</a></li>
-                            <li><a href="#" class="menu__item">Трейлеры</a></li>
-                            <li><a href="#" class="menu__item">Статьи</a></li>
-                            <li class="search"><img src="img/search.png" alt="search "></li>
-                        </ul>
-                    </nav>
-                    <div class="login__register">
-                        <a href="#" class="login">Вход</a>
-                        <a href="#" class="register active">Регистрация</a>
-                    </div>
-                    <div class="burger">
-                        <span>-</span>
-                        <span>-</span>
-                        <span>-</span>
-                    </div>
-                </div>
+                <?php
+                    require_once 'php/nav.php';
+                ?>
             </div>
         </header>
         <main class="page">
---
+++ php/login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/php.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Авторизация</title>
+</head>
+<body>
+    <div class="wrpapper">
+    <header class="header">
+            <div class="container">
+                <div class="header__body">
+                    <div class="header__logo">
+                        <img src="img/svg/logo.svg" alt="logo">
+                    </div>
+                    <nav class="header__nav">
+                        <ul class="menu">
+                            <li><a href="#" class="menu__item">Новости</a></li>
+                            <li><a href="#" class="menu__item">Игры</a></li>
+                            <li><a href="#" class="menu__item">Трейлеры</a></li>
+                            <li><a href="#" class="menu__item">Статьи</a></li>
+                            <li class="search"><img src="img/search.png" alt="search "></li>
+                        </ul>
+                    </nav>
+                    <div class="login__register">
+                        <a href="php/login.php" class="login">Вход</a>
+                        <a href="" class="register active">Регистрация</a>
+                    </div>
+                    <div class="burger">
+                        <span>-</span>
+                        <span>-</span>
+                        <span>-</span>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <main class="page">
+            
+        </main>
+    </div>
+</body>
+</html>
